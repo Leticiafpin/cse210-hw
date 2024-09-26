8fa7966ed41720a8152a42748c62475459ed9c8d
Leticia Figueiredo <pflele1@gmail.com>
2024-09-26 23:41:14 +0100

    week2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        // Create the first job instance
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Display the first job's company
+        job1.DisplayJobInfo();
+
+        // Create the second job instance
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Senior Developer";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        // Display the second job's company
+        job2.DisplayJobInfo();
     }
+
+    
 }
