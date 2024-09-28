688c003d5f1cdadfdd54f7f213e6f6b8b74e8b44
Leticia Figueiredo <pflele1@gmail.com>
2024-09-28 16:24:02 +0100

    corrrection

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,8 +1,9 @@
+using System;
+
 public class Job
 {
-    // Member variables
-    public string _company;
     public string _jobTitle;
+    public string _company;
     public int _startYear;
     public int _endYear;
 
@@ -11,5 +12,3 @@ public class Job
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
-
-
