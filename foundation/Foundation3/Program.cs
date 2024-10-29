bf97f474881a0c165222dac517ef7ab0f9202f70
Leticia Figueiredo <pflele1@gmail.com>
2024-10-29 21:36:06 +0000

    Inheritance and Polymorphism with Exercise Tracking

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,118 @@
 using System;
+using System.Collections.Generic;
+
+abstract class Activity
+{
+    public DateTime Date { get; set; }
+    public int Duration { get; set; } // in minutes
+
+    public Activity(DateTime date, int duration)
+    {
+        Date = date;
+        Duration = duration;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{Date.ToShortDateString()} {this.GetType().Name} ({Duration} min): Distance {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.0} min per km";
+    }
+}
+
+class Running : Activity
+{
+    public double Distance { get; set; } // in kilometers
+
+    public Running(DateTime date, int duration, double distance) : base(date, duration)
+    {
+        Distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return Distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (Distance / Duration) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return Duration / Distance;
+    }
+}
+
+class Cycling : Activity
+{
+    public double Speed { get; set; } // in kilometers per hour
+
+    public Cycling(DateTime date, int duration, double speed) : base(date, duration)
+    {
+        Speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return (Speed * Duration) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return Speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / Speed;
+    }
+}
+
+class Swimming : Activity
+{
+    public int Laps { get; set; }
+
+    public Swimming(DateTime date, int duration, int laps) : base(date, duration)
+    {
+        Laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return (Laps * 50) / 1000.0; // converting meters to kilometers
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / Duration) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return Duration / GetDistance();
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation3 World!");
+
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 4.8),
+            new Cycling(new DateTime(2022, 11, 3), 30, 20),
+            new Swimming(new DateTime(2022, 11, 3), 30, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
